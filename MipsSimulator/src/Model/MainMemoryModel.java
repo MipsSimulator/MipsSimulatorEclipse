bf08b4f337cb5848a5d190327d44d1d5d723c50e
Ben Hunt <wbh26@txstate.edu>
2018-04-21 15:51:05 -0500

    Getters and Setters created for both memmory classes

--- MipsSimulator/src/Model/MainMemoryModel.java
+++ MipsSimulator/src/Model/MainMemoryModel.java
@@ -4,36 +4,41 @@ package Model;
 * Model in the MVC architecture
 * The MemoryModel holds the state of the Assembly Simulator
 */
-public class MemoryModel {
+public class MainMemoryModel {
 
    /**
     * The single instance of the MemoryModel
     */
-   private MemoryModel memoryModel;
+   private MainMemoryModel memoryModel;
+   
+   /**
+    * The main memory. Access is allowed only at every word, which is 32 bits.
+    * This is why the data type is a byte.
+    */
+   private int[] mainMemory;
 
    /**
     * Addressable size of Main Memory. Main memory is 8mb by default.
     * Addressing is only allowed to every word, which is 4 bytes.
-    * Note: The bytes are still there, but the memory is only accessible
-    * in chunks of 4 bytes.
+    * 1024Bytes*1024 = 1Mb*8 = 8mb
     */
-   private static final int MEMORY_SIZE = 1024*1024*8;
+   public static final int MEMORY_SIZE = 1024*1024*8;
 
    /**
     * Top address of the Static Segment. 5.5Mb, or 1024*1024*5.5 = 5767168
     */
-   private static final int STATIC_TOP_ADDRESS = 5767168;
+   public static final int STATIC_TOP_ADDRESS = 5767168;
 
    /**
     * Size of the static segment is 1mb. 5767168 - 1mb = 4718592
     * or 5767168 - 1024*1024 = 4718592
     */
-   private static final int STATIC_BOTTOM_ADDRESS = 4718592;
+   public static final int STATIC_BOTTOM_ADDRESS = 4718592;
 
    /**
     * Size of the text segment is 4mb. 4718592 - 1024*1024*4 or 4mb = 524288
     */
-   private static final int TEXT_BOTTOM_ADDRESS = 524288;
+   public static final int TEXT_BOTTOM_ADDRESS = 524288;
 
    /**
     * Stack Pointer. Holds index (address) to the stack.
@@ -57,20 +62,13 @@ public class MemoryModel {
    private int pc;
 
    /**
-    * The main memory. Access is allowed only at every word, which is 32 bits.
-    * This is why the data type is a byte.
-    */
-   private byte[] mainMemory;
-
-
-   /**
     * Initializes the memory model. Sets the stack pointer, frame pointer, and
     * global pointer to their respective locations in memory.
     */
-   private MemoryModel() {
+   private MainMemoryModel() {
 
-       memoryModel = new MemoryModel();
-       mainMemory = new byte[MEMORY_SIZE];
+       memoryModel = new MainMemoryModel();
+       mainMemory = new int[MEMORY_SIZE];
 
        this.sp = MEMORY_SIZE-1;
        this.gp = STATIC_TOP_ADDRESS;
@@ -86,15 +84,84 @@ public class MemoryModel {
     * to exist at a time)
     * @return
     */
-   public MemoryModel getInstance(){
+   public MainMemoryModel getInstance(){
        if(memoryModel == null){
-           memoryModel = new MemoryModel();
+           memoryModel = new MainMemoryModel();
            return memoryModel;
        }
 
        return memoryModel;
    }
+   
+   
+   /**
+    * 
+    * @return stack pointer
+    */
+   public int getSp() { return this.sp;}
+   /**
+    * 
+    * @return frame pointer
+    */
+   public int getFp() { return this.fp;}
+   /**
+    * 
+    * @return global pointer
+    */
+   public int getGp() { return this.gp;}
+   /**
+    * 
+    * @return program counter
+    */
+   public int getPc() { return this.pc;}
+   
+   
+   /**
+    * Updates the program counter
+    * @param memoryAddress - The address the PC should be set to.
+    */
+   public void setPc(int memoryAddress) { this.pc = memoryAddress ;}
+   
+   /**
+    * Updates the frame pointer
+    * @param memoryAddress
+    */
+   public void setFp(int memoryAddress) {this.fp = memoryAddress; }
+   
 
-
+   /**
+    * Updates the global pointer
+    * @param memoryAddress
+    */
+   public void setGp(int memoryAddress) {this.gp = memoryAddress; }
+   
+   
+   /**
+    * Updates the stack pointer
+    * @param memoryAddress
+    */
+   public void setSp(int memoryAddress) {this.gp = memoryAddress; }
+   
+   
+   
+   
+   
+   /**
+    * [Read Only] Used to read main memory at the given memory address.
+    * This can be used as load word.
+    * @param memoryAddress
+    * @return
+    */
+   public int getMemory(int memoryAddress) { return mainMemory[memoryAddress]; }
+   
+   
+   /**
+    * [Write Only] used to write to memory at the given address.
+    * This can be used as store word.
+    * @param memoryAddress
+    * @return
+    */
+   public int setMemory(int memoryAddress) { return mainMemory[memoryAddress]; }
+   
 
 }
---
+++ MipsSimulator/src/Model/RegisterModel.java
@@ -0,0 +1,72 @@
+package Model;
+
+/**
+ * Models MIP's 32 registers. Each register is 32 bits in width.
+ * @author Ben
+ *
+ */
+public class RegisterModel {
+	
+	/**
+	 * The single RegisterModel instance. (Singleton)
+	 */
+	private RegisterModel registerModel;
+	
+	/**
+	 * int array that is initialized to 32 to hold the MIPS 32 registers.
+	 */
+	private int[] registerFile;
+	
+	private RegisterModel() {
+		registerFile = new int[32];
+		for(int i = 0; i<32; i++)
+			registerFile[i] = 0;
+	}
+	
+	
+	/**
+	 * Singleton method that returns the single instance of RegisterModel
+	 * @return RegisterModel singleton
+	 */
+	public RegisterModel getInstance() {
+		if(registerModel == null) {
+			registerModel = new RegisterModel();
+		}
+		return registerModel;
+	}
+	
+	
+	/**
+	 * Gets the value in the register at the specified register File.
+	 * @param registerIndex
+	 * @return
+	 */
+	public int getRegister(int registerIndex) {
+		
+		return registerFile[registerIndex];
+	}
+	
+	/**
+	 * Updates the register at the given index to the registerFile. Updates it with the data
+	 * that is passed in.
+	 * @param data
+	 * @param registerIndex
+	 */
+	public void setRegister(int data, int registerIndex) {
+		registerFile[registerIndex] = data;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+
+}
